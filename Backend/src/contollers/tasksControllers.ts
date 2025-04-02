ae6e36f4bcf4b65b1fad0915fe05acca0c861cbd
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-04-03 00:04:43 +0530

    ErroR while creating a new task fixed

--- Backend/src/contollers/tasksControllers.ts
+++ Backend/src/contollers/tasksControllers.ts
@@ -3,7 +3,7 @@ import { Tasks } from "../models/taskModel";
 import { authReq } from "../middleware/authMiddleware";
 
 export const newTask= async ( req:authReq,res:Response )=>{
-    const userID=req.user||{};
+    const userID=req.user?.userID||{};
     const {Task,EndDate,Description,Priority}= req.body;
     
     try{
@@ -13,6 +13,7 @@ export const newTask= async ( req:authReq,res:Response )=>{
         }
         const newTask = await Tasks.create({userID:userID,Task:Task,EndDate:EndDate,Description:Description,Priority:Priority})
         res.status(201).json({message:"New Task Creater"});
+        console.log(newTask);
     }catch(err){
         console.log("error",err);
         res.status(500).json({message:"Somethign bad happened",err});
