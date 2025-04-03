41904cd7315e50ec61fc3363083aee2dc71bcde1
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-04-03 15:33:57 +0530

    Task Deleting Functional Added In Backend

--- Backend/src/contollers/tasksControllers.ts
+++ Backend/src/contollers/tasksControllers.ts
@@ -18,6 +18,25 @@ export const newTask= async ( req:authReq,res:Response )=>{
         console.log("error",err);
         res.status(500).json({message:"Somethign bad happened",err});
     }
+}
 
+export const delTask= async(req:Request,res:Response)=>{
+    const {taskID}= req.body;
+    if(!taskID){
+        res.status(401).json({message:"task id required"})
+        return;
+    }
+    try{
+        const deletedtask = await Tasks.findByIdAndDelete(taskID);
+
+        if(!deletedtask){
+            res.status(404).json({message:"task with the id does not exist"});
+        }
+
+        res.status(200).json({message:"task is delted successfully"})
+    }
+    catch(err){
+        res.status(402).json({"Error: ":err})
+    }
 
 }
--- Backend/src/routes/taskroutes.ts
+++ Backend/src/routes/taskroutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { newTask } from "../contollers/tasksControllers";
+import { newTask,delTask } from "../contollers/tasksControllers";
 import { auth } from "../middleware/authMiddleware";
+
 
 const router = express.Router();
 
 router.post("/newtask",auth,newTask)
+router.delete("/deletetask",delTask)
 
 
 export default router; 
