11921b183da75e61d57cee4febf8c4dd5e385a79
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-04-06 23:38:24 +0530

    UI changes

--- Frontend/src/app/login/components/Login.tsx
+++ Frontend/src/app/login/components/Login.tsx
@@ -48,24 +48,23 @@ export const Login= ({onSubmit}:submition) => {
 
 
     return(
-        <div className="bg-[#101010] w-screen h-screen flex flex-col items-center">
-        <h1 className="w-[60%] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent h-24 font-bold text-5xl text-center p-4">Task Manager & Scheduler</h1>
-        <h3 className="font-semibold text-xl text-white/30 w-[60%] h-12 -mt-4 text-center">Streamline your productivity with modern task management</h3>
-        <div className="w-[30%] h-[80%] bg-black/20 border border-gray-50/20  rounded-2xl p-2 flex flex-col items-center mb-4">
-          <h2 className=" bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text font-bold text-3xl mt-2">Task Manager & Scheduler</h2>
-          <h3 className="text-white mb-8">Manage tasks, schedules, and boost your productivity</h3>
-          <form className="w-full flex flex-col items-center">
-            {InputFieldsData.map((items, index) => (<InputFields key={index} {...items} value={data[items.name]||""} onChange={dataHandler}/>))}
-          </form>
-          <hr className="w-[90%] h-1 text-white/30 mt-2 mb-4" />
-          <button className="w-[90%] h-12 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 rounded-xl text-center text-white font-bold text-2xl" onClick={handleSignup}>Sign UP</button>
-          <div className="w-[90%] flex justify-center items-center mt-4">
-                <h3 className="text-lg text-white ">Already have an account</h3>
-                <span className="text-blue-400 text-lg ml-2 font-semibold">Log In...</span>
-          </div>
+        <div className="relative bg-[#101010] w-screen h-screen flex flex-col items-center">
+            <h1 className="w-[60%] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent h-24 font-bold text-5xl text-center p-4">Task Manager & Scheduler</h1>
+            <h3 className="font-semibold text-xl text-white/30 w-[60%] h-12 -mt-4 text-center">Streamline your productivity with modern task management</h3>
+            <div className="w-[30%] h-[80%] bg-black/20 border-2 border-dashed border-gray-50/20  rounded-2xl p-2 flex flex-col items-center mb-4 ">
+                <h2 className=" bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text font-bold text-3xl mt-2">Task Manager & Scheduler</h2>
+                <h3 className="text-white mb-8">Manage tasks, schedules, and boost your productivity</h3>
+                <form className="w-full flex flex-col items-center">
+                    {InputFieldsData.map((items, index) => (<InputFields key={index} {...items} value={data[items.name]||""} onChange={dataHandler}/>))}
+                </form>
+                <hr className="w-[90%] h-1 text-white/30 mt-2 mb-4" />
+                <button className="w-[90%] h-12 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 rounded-xl text-center text-white font-bold text-2xl" onClick={handleSignup}>Sign UP</button>
+                <div className="w-[90%] flex justify-center items-center mt-4">
+                    <h3 className="text-lg text-white ">Already have an account</h3>
+                    <span className="text-blue-400 text-lg ml-2 font-semibold">Log In...</span>
+                </div>
+            </div>
         </div>
-
-      </div>
     )
 }
 
